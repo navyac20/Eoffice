2fbb61b4b44da0333a282ee21b0b4f1a7e5d94df
navyac20 <93192003+navyac20@users.noreply.github.com>
2024-03-15 16:30:26 +0530

    projectt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import Login from './Login'
+import React from 'react';
+import Login from './Login';
+
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signup from './Signup';
+import Home from './home'; // Update the import statement
 
 function App() {
   return (
-    <div>
-      <Login></Login>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
